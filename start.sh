1fbbb7468d5ebf5adea057d56c391d8d1c8adc95
ojcarrillo <oj_carrillo@yahoo.com>
2018-02-27 17:06:15 +0000

    ajuste para el volumen del ftp

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ echo "corre imagen del contenedor"
 docker run --name dk_fstransporte \
 -d -p 2021:21 -p 2020:20 -p 12020:12020 -p 12021:12021 -p 12022:12022 -p 12023:12023 -p 12024:12024 -p 12025:12025  \
 -e "USER=touresbalon" -e "PASS=verysecretpwd" \
--v /data/ftp:/ftp \
+-v /data/ftp/fstransporte:/ftp \
 -it dk_fstransporte
 echo "======================================================="
 echo "======================================================="
